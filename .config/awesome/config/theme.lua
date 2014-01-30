30b0c2b6d34dd1f8389af9fb54d830960e0fad73
Sergey Yakovlev <sadhooklay@gmail.com>
2014-01-31 01:03:46 +0400

    Cleanup main Awesome WM config, moved layouts and tags into subconfig

--- .config/awesome/config/theme.lua
+++ .config/awesome/config/theme.lua
@@ -7,5 +7,15 @@
 
 --]]--
 
+local gears = require("gears")
+
 -- Define colours, icons, and wallpapers
 beautiful.init(__dir__ .. "/themes/copyburn/theme.lua")
+
+
+-- Wallpaper
+if beautiful.wallpaper then
+  for s = 1, screen.count() do
+    gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+  end
+end
