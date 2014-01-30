55af2b165df0e5b1e0bb3995e6609f737b72ba58
Sergey Yakovlev <sadhooklay@gmail.com>
2014-01-30 23:30:01 +0400

    Initial commit fo Theme
    Part of Awesome WM config

---
+++ .config/awesome/config/theme.lua
@@ -0,0 +1,11 @@
+--[[--
+
+  Theme handling
+
+  Part of Awesome WM config
+  by Sergey Yakovlev (me@klay.me)
+
+--]]--
+
+-- Define colours, icons, and wallpapers
+beautiful.init(__dir__ .. "/themes/copyburn/theme.lua")
