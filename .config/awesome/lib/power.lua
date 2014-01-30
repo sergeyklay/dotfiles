0fa8bd1f02d172615cefec8803e59ea74e0eb09b
Serghei Iakovlev <egrep@protonmail.ch>
2014-01-31 02:56:44 +0400

    Shorter functions for Awesome power management lib

--- .config/awesome/lib/power.lua
+++ .config/awesome/lib/power.lua
@@ -12,30 +12,31 @@
 --]]--
 
 local awful = require("awful")
+local run   = awful.util.spawn_with_shell
 
 module("lib.power")
 
 -- Suspend the system
 function suspend()
-  awful.util.spawn_with_shell("zenity --question --text 'Suspend the system' && systemctl suspend")
+  run("zenity --question --text 'Suspend the system' && systemctl suspend")
 end
 
 -- Put the system into hibernation
 function hibernate()
-  awful.util.spawn_with_shell("zenity --question --text 'Put the system into hibernation' && systemctl hibernate")
+  run("zenity --question --text 'Put the system into hibernation' && systemctl hibernate")
 end
 
 -- Put the system into hybrid-sleep state (or suspend-to-both)
 function sleep()
-  awful.util.spawn_with_shell("zenity --question --text 'Sleep the system' && systemctl hibrid-sleep")
+  run("zenity --question --text 'Sleep the system' && systemctl hibrid-sleep")
 end
 
 -- Shut down and reboot the system
 function reboot()
-  awful.util.spawn_with_shell("zenity --question --text 'Shut down and reboot the system' && systemctl reboot")
+  run("zenity --question --text 'Shut down and reboot the system' && systemctl reboot")
 end
 
 -- Shut down and power-off the system
 function poweroff()
-  awful.util.spawn_with_shell("zenity --question --text 'Shut down and power-off the syste' && systemctl poweroff")
+  run("zenity --question --text 'Shut down and power-off the syste' && systemctl poweroff")
 end
