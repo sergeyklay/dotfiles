0637b9783f6d8a1fcca3d511ca1732bab5730419
Serghei Iakovlev <egrep@protonmail.ch>
2019-06-22 14:23:31 +0300

    Remove no longer needed user.clj

--- .lein/user.clj
+++
@@ -1,5 +0,0 @@
-;; Include clojure 1.3.0 libraries for GNU Emacs clojure-jack-in
-(if (>= (.compareTo (clojure-version) "1.3.0") 0)
-  (do (use 'clojure.repl)
-      (use 'clojure.java.javadoc)
-      (use 'clojure.reflect)))
