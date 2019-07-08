c71bf3141f9305b6f1039d93e456a3972af5b70a
Serghei Iakovlev <egrep@protonmail.ch>
2019-07-08 22:57:52 +0300

    Use '--force' option to update Code extensions

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-while read ext; do code --install-extension "$ext" ; done < code-list-extensions
+while read ext; do
+  code --install-extension "$ext" --force ;
+done < code-list-extensions
