461799a3f49f53c1813022ee76d691592dc91a10
Serghei Iakovlev <sadhooklay@gmail.com>
2019-07-09 20:03:33 +0300

    Remove ultra, flipp
    Refs:
    - https://github.com/brandonbloom/fipp/issues/60
    - https://github.com/venantius/ultra/issues/103

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:plugins [[venantius/ultra "0.6.0"]]}}
+{:user {:dependencies [[com.bhauman/rebel-readline "0.1.4"]]
+        :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}}
