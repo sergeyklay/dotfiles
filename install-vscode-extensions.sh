51c4491ff09c8678f13d3bd159b4fcb3116b18db
Serghei Iakovlev <sadhooklay@gmail.com>
2019-07-19 10:44:26 +0300

    Improved extesions installer for VS Code

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+>&1 printf "===========================================\n"
+>&1 printf "Installing or updating extensions...\n"
+>&1 printf "===========================================\n\n"
+
 while read ext; do
   code --install-extension "$ext" --force ;
 done < code-list-extensions
+
+>&1 printf "\n===========================================\n"
+>&1 printf "Uninstalling no longer used extensions...\n"
+>&1 printf "===========================================\n\n"
+
+for ext in $(code --list-extensions); do
+  grep "${ext}" code-list-extensions 1>/dev/null || \
+    code --uninstall-extension "${ext}"
+done
