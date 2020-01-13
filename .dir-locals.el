a7e5ab1412fa6b557ea25ef6b7e2022c508a399b
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-14 00:03:54 +0200

    Set directory local variables, correct git editor

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((indent-tabs-mode . nil)
+	 (fill-column . 78)))
+ (sh-mode . ((sh-indentation . 2)))
+ (gitconfig-mode . ((tab-width . 2)
+                    (indent-tabs-mode . t))))
