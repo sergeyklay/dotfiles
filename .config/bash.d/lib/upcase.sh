36bc3a159dcd83a3c3490796a277dcabbbae1389
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-07 09:47:01 +0200

    Add upcase function

---
+++ .config/bash.d/lib/upcase.sh
@@ -0,0 +1,29 @@
+# Copyright (C) 2014-2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+# shellcheck shell=bash
+
+function upcase() {
+  local p
+
+  # shellcheck disable=SC2016
+  p='{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1'
+
+  echo -n "$1" | awk "$p"
+}
+
+# Local Variables:
+# mode: sh
+# End:
