a12a127700573bc2b59518721c0ec62d903df7bf
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-08 19:46:30 +0200

    Move 3rd party plugins to a separated place

---
+++ .config/bash.d/lib/phpenv_init.sh
@@ -0,0 +1,29 @@
+# Copyright (C) 2014-2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+# shellcheck shell=bash
+
+function phpenv_init {
+  if command -v phpenv >/dev/null 2>&1 ; then
+    eval "$(phpenv init -)"
+  else
+    >&2 echo "command phpenv not found"
+    return 1
+  fi
+}
+
+# Local Variables:
+# mode: sh
+# End:
