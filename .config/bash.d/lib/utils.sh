456d40300956a1183e03f0620f0da98a17da8948
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-22 01:27:03 +0200

    Setup list of directories to search for Info documentation files

---
+++ .config/bash.d/lib/utils.sh
@@ -0,0 +1,34 @@
+# Copyright (C) 2014-2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+# shellcheck shell=bash
+
+# Checks if a value exists in an array.
+#
+#     # Check if the array has a certain key:
+#     $ in_array "key" "${!array[@]}"
+#
+#     # Check if the array contains a certain value:
+#     $ in_array "key" "${array[@]}"
+#
+in_array() {
+  local p
+
+  for p in "${@:2}"; do
+    [[ "$p" = "$1" ]] && return 0
+  done
+
+  return 1
+}
