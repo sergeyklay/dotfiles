e0dd78bc19cc55b4d85d71118979572f7f31f632
Serghei Iakovlev <egrep@protonmail.ch>
2021-06-05 19:30:29 +0300

    Allow custom paths for pyenv

--- .config/bash.d/plugins/plugin.pyenv.sh
+++ .config/bash.d/plugins/plugin.pyenv.sh
@@ -26,19 +26,26 @@ autoload warn
 
 # Meant for non-interactive login shells.
 _plugin_pyenv_login() {
-  if [ -z ${PYENV_ROOT+x} ] && [ -d "$HOME/.pyenv" ]; then
-    PYENV_ROOT="$HOME/.pyenv"
-    export PYENV_ROOT
-  fi
+  for dir in ~/.pyenv /opt/homebrew/opt/pyenv /usr/local/opt/pyenv
+  do
+    if [[ -d "$dir" ]]; then
+      PYENV_ROOT="$dir"
+      export PYENV_ROOT
 
-  if [ -n "$PYENV_ROOT" ] && [ -d "$PYENV_ROOT/bin" ]; then
-    pathmunge "$PYENV_ROOT/bin"
-  fi
+      if [[ -d "$dir/bin" ]]; then
+        pathmunge "$dir/bin"
+      fi
+
+      break
+    fi
+  done
 
   if [ -z ${PYENV_SHELL+x} ]; then
     if command -v pyenv >/dev/null 2>&1; then
       eval "$(pyenv init --path)"
 
+      # Automatically activate a Python virtualenv environment
+      # based on current pyenv version.
       if pyenv commands | grep -q virtualenv-init ; then
         eval "$(pyenv virtualenv-init -)"
       fi
@@ -53,7 +60,6 @@ _plugin_pyenv_login() {
 _plugin_pyenv() {
   if [ -z ${PYENV_ROOT+x} ] || ! command -v pyenv >/dev/null 2>&1
   then
-    echo "$PYENV_ROOT"
     warn "pyenv plugin is not configured"
     return 1
   fi
