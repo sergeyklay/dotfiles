1c9aec3a1e15316697904e80aa23182b0bc56044
Serghei Iakovlev <git@serghei.pl>
2024-10-15 21:06:28 +0200

    Improve getting ip address

--- .config/waybar/modules/essid.sh
+++ .config/waybar/modules/essid.sh
@@ -3,15 +3,19 @@
 # Get the current Wi-Fi ESSID
 essid=$(nmcli -t -f active,ssid dev wifi | awk -F: '/^yes/ {print $2}')
 
-interface=""
-ip_address=""
-
 # If no ESSID is found, set a default value
 if [ -z "$essid" ]; then
   echo "No Connection"
 else
-  interface=$(ip route | grep '^default' | awk '{print $5}')
-  ip_address=$(ip addr show "$interface" | grep 'inet ' | awk '{print $2}')
+  ip_address="127.0.0.1"
+  active_device=$(nmcli -t -f DEVICE,STATE device status | \
+    grep -w "connected" | \
+    grep -v -E "^(dummy|lo:)" | \
+    awk -F: '{print $1}')
+
+  if [ -n "$active_device" ]; then
+    ip_address=$(nmcli -g IP4.ADDRESS device show $active_device)
+  fi
 
   echo "${essid}: ${ip_address}"
 fi
