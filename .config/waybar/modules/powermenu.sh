42e296fc04b8ee19c3768cc487ff7a799a0f9163
Serghei Iakovlev <git@serghei.pl>
2024-10-30 00:29:23 +0100

    Remove old menu stuff

--- .config/waybar/modules/powermenu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-username="$(whoami)"
-realname="$(getent passwd "${username}" | cut -d ':' -f 5 | cut -d ',' -f 1)"
-class=powermenu
-
-echo -e "{\"text\":\""$realname"\", \"class\":\""$class"\"}"
